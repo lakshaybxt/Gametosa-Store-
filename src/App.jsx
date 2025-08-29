3647c075085931c29638ffcd4c76639d3fa1c7dd
Adarsh Mishra <adarshofficial24@gmail.com>
2025-08-29 17:57:56 +0530

    Initial project structure - React + Vite app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoadingPage from './pages/LoadingPage';
+// import './styles/main.css'; // Make sure this imports your Tailwind CSS
+
+function App() {
+  return (
+    <>
+      <LoadingPage />
+    </>
+  );
+}
+
+export default App;
