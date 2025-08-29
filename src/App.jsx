e66607680d5ba47c1a346731b3552e47f04526c9
Adarsh Mishra <adarshofficial24@gmail.com>
2025-08-29 18:38:13 +0530

    setup is ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import LoadingPage from './pages/LoadingPage';
-// import './styles/main.css'; // Make sure this imports your Tailwind CSS
 
 function App() {
   return (
